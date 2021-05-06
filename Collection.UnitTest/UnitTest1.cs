d6e111cfbceb0a7308a8dcb4f0eae563ac6cc86b
Manuela Trotta <MTrotta@qubicaamf.com>
2021-05-06 12:01:42 +0200

    inserimento package NUnit e modifica su Assert in test 1

--- Collection.UnitTest/UnitTest1.cs
+++ Collection.UnitTest/UnitTest1.cs
@@ -9,24 +9,52 @@ namespace Collection.UnitTest
         {
         }
 
-        [Test]
-        public void CancelledBy_UserisAdmin_Expected_ReturnTrue()
-        {
+        [TestFixture]
+        public class DeclarationTest
+        {    [Test]
+            public void CancelledBy_UserisAdmin_Expected_ReturnTrue()
+            {
 
-            // arrange parte dove si inizializza l'oggetto da testare aggiungere riferimento
+                // arrange parte dove si inizializza l'oggetto da testare aggiungere riferimento
 
-            var reservation = new Reservation();
-
-
-            //act  parte dove si testa il metodo
-
-            var result = reservation.CancelledBy(new User { IsAdmin = true });
+                var reservation = new Reservation();
 
 
-            //assert parte in cui si verifica se il risultato è corretto in questo caso deve essere true uso isTrue metodo 
+                //act  parte dove si testa il metodo
+
+                var result = reservation.CancelledBy(new User { IsAdmin = true });
 
 
-            Assert.IsTrue(result);
+                //assert parte in cui si verifica se il risultato è corretto in questo caso deve essere true uso isTrue metodo 
+
+
+                Assert.That(result == true);
+            }
+            [Test]
+            public void CancelledBy_MadeBy_Expected_ReturnTrue()
+            {
+
+                // creo sia la reservation che l'user nella reservation imposto che è stata effettuata dall'user 
+                var user = new User { };
+                var reservation = new Reservation { MadeBy = user };
+
+
+                var result1 = reservation.CancelledBy(user);
+
+                Assert.IsTrue(result1);
+            }
+
+            [Test]
+            public void CancelledBy_NoAdminMadeBy_ReturnFalse()
+            {
+                var user = new User();
+                var reservation = new Reservation();
+
+                var result = reservation.CancelledBy(user);
+
+                Assert.False(result);
+
+            }
         }
     }
 }
